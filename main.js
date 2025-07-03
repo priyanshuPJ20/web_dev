595b4749d56ee9aeba8eb6f1c77f59217a30c366
priyanshuPJ20 <priyanshu19.kpl@gmail.com>
2025-07-03 22:06:31 +0530

    second commit

---
+++ main.js
@@ -0,0 +1,10 @@
+function print(message){
+    console.log(message)
+}
+function printTimes(message,times){
+    for(let i=0;i<times;i++)
+        console.log(message)
+}
+printTimes("priyanshu",5);
+print("Hello World")
+print("bye")
